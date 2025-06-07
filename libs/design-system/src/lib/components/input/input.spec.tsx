7f61fd0b52517fc2ff378c4f5d8692b3c84bf529
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-06-07 18:01:59 -0300

    feat: insert input component

---
+++ libs/design-system/src/lib/components/input/input.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BytebankInputController } from './ControlledInput';
+import { useForm, FormProvider } from 'react-hook-form';
+
+const Wrapper: React.FC<React.PropsWithChildren<object>> = ({ children }) => {
+  const methods = useForm();
+
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
+
+describe('Input', () => {
+  it('render bytebankInput component correctly', () => {
+    const { baseElement } = render(
+      <Wrapper>
+        <BytebankInputController name="nome" label="nome" />
+      </Wrapper>
+    );
+    const inputElement = baseElement.querySelector('.bytebank-input');
+    expect(inputElement).toHaveTextContent('nome');
+  });
+});
