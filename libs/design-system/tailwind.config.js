9f7f697f5a5fada94518303313099fc6c9669c91
Daniela Farias <daniela.pereira@picpay.com>
2025-06-10 00:30:25 -0300

    core: created design system lib with tailwind

---
+++ libs/design-system/tailwind.config.js
@@ -0,0 +1,35 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: 'var(--primary-light)',
+          DEFAULT: 'var(--primary)',
+          dark: 'var(--primary-dark)',
+        },
+        secondary: {
+          light: 'var(--secondary-light)',
+          DEFAULT: 'var(--secondary)',
+          dark: 'var(--secondary-dark)',
+        },
+        white: 'white',
+        black: 'var(--black)',
+      },
+      spacing: {
+        sm: 'var(--spacing-sm)',
+        md: 'var(--spacing-md)',
+        lg: 'var(--spacing-lg)',
+        xl: 'var(--spacing-xl)',
+      }
+    },
+  },
+  plugins: [require('flowbite')],
+};
