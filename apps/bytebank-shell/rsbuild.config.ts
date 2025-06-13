8961572c4afa82da19237c9f0ab1d6e8b2984e5d
Daniela Farias <daniela.pereira@picpay.com>
2025-06-13 19:28:07 -0300

    chore: prettier lint

--- apps/bytebank-shell/rsbuild.config.ts
+++ apps/bytebank-shell/rsbuild.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig, loadEnv } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
-import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
-import { pluginSass } from '@rsbuild/plugin-sass';
-import mfConfig from './module-federation.config';
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import { pluginSass } from "@rsbuild/plugin-sass";
+import mfConfig from "./module-federation.config";
 
-const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
+const { publicVars } = loadEnv({ prefixes: ["REACT_APP_"] });
 
 export default defineConfig({
   html: {
     template: "./public/index.html",
   },
-  plugins: [pluginReact(), pluginSvgr({ mixedImport: true }),pluginModuleFederation(mfConfig), pluginSass()],
+  plugins: [
+    pluginReact(),
+    pluginSvgr({ mixedImport: true }),
+    pluginModuleFederation(mfConfig),
+    pluginSass(),
+  ],
   source: {
     // Compile all JS files and exclude core-js
     include: [{ not: /[\\/]core-js[\\/]/ }],
--- apps/mfe-investments/rsbuild.config.ts
+++ apps/mfe-investments/rsbuild.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig, loadEnv } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginSvgr } from "@rsbuild/plugin-svgr";
-import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
-import mfConfig from './module-federation.config';
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import mfConfig from "./module-federation.config";
 import { pluginSass } from "@rsbuild/plugin-sass";
 
 const { publicVars } = loadEnv({ prefixes: ["REACT_APP_"] });
 
 export default defineConfig({
   server: {
-    port: 3001
+    port: 3001,
   },
   html: {
     template: "./public/index.html",
   },
-  plugins: [pluginReact(), pluginSvgr({ mixedImport: true }), pluginModuleFederation(mfConfig), pluginSass()],
+  plugins: [
+    pluginReact(),
+    pluginSvgr({ mixedImport: true }),
+    pluginModuleFederation(mfConfig),
+    pluginSass(),
+  ],
   source: {
     // Compile all JS files and exclude core-js
     include: [{ not: /[\\/]core-js[\\/]/ }],
