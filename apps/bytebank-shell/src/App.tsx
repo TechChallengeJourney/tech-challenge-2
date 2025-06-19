ca57298a8689b63ccc5ffb649ff398eb98868d9d
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-06-18 22:07:38 -0300

    fix

--- apps/bytebank-shell/src/App.tsx
+++ apps/bytebank-shell/src/App.tsx
@@ -6,48 +6,47 @@ import {
   BytebankInput,
   BytebankSelect,
 } from "@repo/ui";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
     <>
       <AppRoutes />
-      <br />
-      <br />
-      <BytebankText variant="h1" color="primary">
-        Componente de texto da ui
-      </BytebankText>
-      <BytebankText variant="md" color="secondary">
-        Componente de texto da ui
-      </BytebankText>
-      <br />
-      <br />
-      <BytebankDivider type="horizontal" color="primary" />
-      <br />
-      <BytebankInput
-        label="Valor"
-        value={""}
-        onChange={() => {}}
-        type="text"
-        placeholder="Digite o valor"
-        error={false}
-        helperText="Digite o valor da transação"
-        autoComplete="off"
-        mask="currency"
-      />
-      <br />
-      <BytebankSelect
-        value={""}
-        onChange={() => {}}
-        label="Selecione uma opção"
-        options={[
-          { label: "Opção 1", value: "opcao1" },
-          { label: "Opção 2", value: "opcao2" },
-          { label: "Opção 3", value: "opcao3" },
-        ]}
-        error={false}
-        helperText="Selecione uma opção"
-      />
-      <br />
+      <Box display="flex" flexDirection="column" gap={2} p={2}>
+        <BytebankText variant="h1" color="primary">
+          Componente de texto da ui
+        </BytebankText>
+        <BytebankText variant="md" color="secondary">
+          Componente de texto da ui
+        </BytebankText>
+
+        <BytebankDivider type="horizontal" color="primary" />
+
+        <BytebankInput
+          label="Valor"
+          value={""}
+          onChange={() => {}}
+          type="text"
+          placeholder="Digite o valor"
+          error={false}
+          helperText="Digite o valor da transação"
+          autoComplete="off"
+          mask="currency"
+        />
+
+        <BytebankSelect
+          value={""}
+          onChange={() => {}}
+          label="Selecione uma opção"
+          options={[
+            { label: "Opção 1", value: "opcao1" },
+            { label: "Opção 2", value: "opcao2" },
+            { label: "Opção 3", value: "opcao3" },
+          ]}
+          error={false}
+          helperText="Selecione uma opção"
+        />
+      </Box>
     </>
   );
 }
