2e58e196db90bd19a8a3c8f8a28000f70adaf309
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-07-08 21:56:10 -0300

    feat: styles improvements

--- packages/ui/src/components/card-bank/index.tsx
+++ packages/ui/src/components/card-bank/index.tsx
@@ -2,7 +2,6 @@ import { Box, CardContent } from "@mui/material";
 import { colorsPalette, useTheme } from "@repo/utils";
 import { BankCardVariant } from "../../classes";
 import { BytebankCard } from "../card/card";
-import { BytebankIllustration } from "../illustration/illustration";
 import { BytebankText } from "../text/text";
 
 import "./style.scss";
@@ -22,7 +21,6 @@ export function BytebankCardBank({
   variant = "Físico",
   details,
 }: BytebankCardBankProps) {
-  
   const { isDarkMode } = useTheme();
   const palette = !isDarkMode ? colorsPalette.light : colorsPalette.dark;
   const bgcolor: string | undefined =
@@ -33,28 +31,39 @@ export function BytebankCardBank({
       <BytebankCard
         bgcolor={bgcolor}
         variant="elevation"
-        className={`card-bank--container__card`}
+        className="card-bank--container__card"
       >
-        <>
-          <Box padding={2} gap={2} color="#FFF">
-            <CardContent>
-              <Box display="flex" flexDirection={"column"}>
-                <BytebankIllustration name="platinum" variant="auto" />
-                <Box display="flex" flexDirection={"column"} marginTop={4}>
-                  <BytebankText variant="sm" fontWeight={500} paddingBottom={1}>
-                    **** **** **** {details.cardNumber.slice(-4)}
-                  </BytebankText>
-                  <BytebankText variant="sm" fontWeight={200}>
-                    {details.name}
-                  </BytebankText>
-                  <BytebankText variant="sm" fontWeight={200}>
-                    {details.expirationDate}
-                  </BytebankText>
-                </Box>
+        <Box padding={2} gap={2}>
+          <CardContent>
+            <Box display="flex" flexDirection="column">
+              <BytebankText
+                variant="md"
+                fontWeight={600}
+                style={{ fontStyle: "italic" }}
+              >
+                Byte
+              </BytebankText>
+              <BytebankText variant="xs" fontWeight={400} marginBottom={2}>
+                Platinum
+              </BytebankText>
+
+              <Box
+                width={{ xs: "80%", sm: "60%", md: "40%" }}
+                maxWidth={500}
+                marginY={2}
+              ></Box>
+
+              <Box display="flex" flexDirection="column" marginTop={1}>
+                <BytebankText variant="sm" fontWeight={200} marginBottom={4}>
+                  {details.name}
+                </BytebankText>
+                <BytebankText variant="sm" fontWeight={200}  marginTop={4}>
+                  **** **** **** {details.cardNumber.slice(-4)}
+                </BytebankText>
               </Box>
-            </CardContent>
-          </Box>
-        </>
+            </Box>
+          </CardContent>
+        </Box>
       </BytebankCard>
     </Box>
   );
