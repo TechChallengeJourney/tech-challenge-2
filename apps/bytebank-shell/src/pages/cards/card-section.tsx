f6a13790c9a6e0651ca4895385392d7c946fe95f
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-07-10 21:25:29 -0300

    feat: desktop layout

--- apps/bytebank-shell/src/pages/cards/card-section.tsx
+++ apps/bytebank-shell/src/pages/cards/card-section.tsx
@@ -1,91 +1,119 @@
 import React from "react";
-import { Box } from "@mui/material";''
 import {
   BytebankCardBank,
   BytebankCard,
   BytebankText,
   BytebankButton,
 } from "@repo/ui";
+import { Box } from "@mui/material";
 
 export const BytebankCardSection: React.FC = () => {
+
+
+
   const cardDetails = () => {
     return (
-      <Box display="flex" flexDirection="column" gap={2} p={2} flex={1}>
-        <Box display="flex" justifyContent="space-between">
+      <>
+        <Box display="flex" justifyContent="space-between" width="500px">
           <BytebankText variant="xs">Cartão de crédito</BytebankText>
           <BytebankText variant="xs">**** **** **** 3251</BytebankText>
         </Box>
+
         <Box display="flex" justifyContent="space-between">
           <BytebankText variant="xs">Limite do cartão</BytebankText>
-          <BytebankText variant="xs">R$ 35.000,00</BytebankText>
+          <BytebankText variant="xs" fontWeight="bold">
+            R$ 35.000,00
+          </BytebankText>
         </Box>
+
         <Box display="flex" justifyContent="space-between">
           <BytebankText variant="xs">Data de expiração</BytebankText>
-          <BytebankText variant="xs">25/03/2028</BytebankText>
+          <BytebankText variant="xs" fontWeight="bold">
+            25/03/2028
+          </BytebankText>
         </Box>
+
         <Box display="flex" justifyContent="space-between">
           <BytebankText variant="xs">Valor utilizado</BytebankText>
-          <BytebankText variant="xs">R$1.759,20</BytebankText>
+          <BytebankText variant="xs" fontWeight="bold">
+            R$1.759,20
+          </BytebankText>
         </Box>
-
-        <Box mt={3}>
-          <BytebankButton
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={() => console.log("true")}
-            label="Bloquear cartão"
-          />
-        </Box>
-
-        <Box>
-          <BytebankButton
-            fullWidth
-            variant="outlined"
-            color="white"
-            onClick={() => console.log("true")}
-            label="Marcar cartão como principal"
-          />
-        </Box>
-
-        <Box textAlign="center" mt={1}>
-          <BytebankButton
-            label="Excluir cartão"
-            onClick={() => console.log("true")}
-            sx={{ my: 2, display: "block", textTransform: "capitalize" }}
-            variant="text"
-            color={"primary"}
-          />
-        </Box>
-      </Box>
+      </>
     );
   };
 
+   const buttonsWrapper = () => {
+    return (
+      <>
+        <Box
+          mt={{ xs: 3, md: 3 }}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Box width="100%">
+            <BytebankButton
+              fullWidth
+              variant="contained"
+              color="primary"
+              label="Bloquear cartão"
+              onClick={() => console.log("Bloquear cartão")}
+            />
+          </Box>
+
+          <Box mt={2} width="100%">
+            <BytebankButton
+              fullWidth
+              color="primary"
+              variant="outlined"
+              label="Marcar cartão como principal"
+              onClick={() => console.log("Marcar como principal")}
+            />
+          </Box>
+
+          <Box mt={2}>
+            <BytebankButton
+              label="Excluir cartão"
+              color="primary"
+              variant="text"
+              onClick={() => console.log("Excluir cartão")}
+              sx={{
+                textDecoration: "underline",
+                textTransform: "none",
+                fontWeight: 500,
+              }}
+            />
+          </Box>
+        </Box>
+      </>
+    );
+  };
+
+ 
   const cards = () => {
     return (
-      <Box flex={1} p={2}>
-        <BytebankCardBank
-          variant="Físico"
-          details={{
-            name: "Eloisa Fagundes",
-            cardNumber: "1234567890123456",
-            expirationDate: "12/25",
-          }}
-        />
-      </Box>
+      <BytebankCardBank
+        variant="Físico"
+        details={{
+          name: "Eloisa Fagundes",
+          cardNumber: "1234567890123456",
+          expirationDate: "12/25",
+        }}
+      />
     );
   };
 
   return (
-    <BytebankCard variant={"outlined"} sx={{ width: "100% !important" }}>
-      <Box
-        display="flex"
-        flexDirection={{ xs: "column", md: "row" }}
-        justifyContent="space-between"
-        width="100%"
-      >
-        {cards()}
-        {cardDetails()}
+    <BytebankCard variant="outlined">
+      <Box display="flex" gap={2} flexDirection={{ xs: "column", md: "row" }}>
+        <Box flex={1} m={{ xs: 1, md: 4 }} alignContent="center">
+          {cards()}
+        </Box>
+        <Box flex={1} m={{ xs: 1, md: 4 }}>
+          {cardDetails()}
+          {buttonsWrapper()}
+        </Box>
       </Box>
     </BytebankCard>
   );
--- apps/bytebank-shell/src/pages/cards/cards.tsx
+++ apps/bytebank-shell/src/pages/cards/cards.tsx
@@ -4,20 +4,8 @@ import { BytebankCardSection } from "./card-section";
 
 export const BytebankCardsPage: React.FC = () => {
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      flexGrow={1}
-      gap={2}
-      minHeight="100vh"
-      px={{ xs: "2", sm: "4", md: "6" }}
-      p={4}
-      boxSizing="border-box"
-      // textAlign="center"
-    >
+    <div >
       <BytebankCardSection />
-    </Box>
+    </div>
   );
 };
