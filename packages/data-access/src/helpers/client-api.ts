671f148e03804b0c5b1a07dace6c235c348ab18a
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-07-14 14:33:21 -0300

    fix: to avoid multiple calls

--- packages/data-access/src/helpers/client-api.ts
+++ packages/data-access/src/helpers/client-api.ts
@@ -1,6 +1,10 @@
 
-import axios from 'axios';
+import axios from "axios";
+import { tokenInterceptor } from "./token-interceptor";
 
 export const clientApi = axios.create({
   baseURL: process.env.PUBLIC_API_URL,
 });
+
+
+tokenInterceptor(clientApi);
