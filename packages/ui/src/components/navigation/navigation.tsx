8456d83186791738ffa646c107b175a210e3d147
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-07-21 22:11:24 -0300

    refactor: change arrow icon

--- packages/ui/src/components/navigation/navigation.tsx
+++ packages/ui/src/components/navigation/navigation.tsx
@@ -1,36 +1,50 @@
 import React from "react";
 import { Box, IconButton } from "@mui/material";
 import {
-  ArrowCircleLeftRounded,
-  ArrowCircleRightRounded,
+  ArrowBackIosNewRounded,
+  ArrowForwardIosRounded,
 } from "@mui/icons-material";
-
 export interface BytebankNavigationProps {
   onPrev: () => void;
   onNext: () => void;
   size?: "inherit" | "large" | "medium" | "small";
-  color?:
-    | "primary"
-    | "secondary"
-    | "success"
-    | "error"
-    | "info"
-    | "warning"
+  color?: "primary" | "secondary" | "success" | "error" | "info" | "warning";
 }
 
 export const BytebankNavigation: React.FC<BytebankNavigationProps> = ({
   onPrev,
   onNext,
   size = "large",
-  color = "primary"
+  color = "primary",
 }) => {
   return (
     <Box display="flex" gap={1}>
-      <IconButton onClick={onPrev} size="small">
-        <ArrowCircleLeftRounded fontSize={size} color={color} />
+      <IconButton
+        onClick={onPrev}
+        size="medium"
+        sx={{
+          borderRadius: "50%",
+          bgcolor: "background.paper",
+          "&:hover": {
+            bgcolor: "action.hover",
+          },
+        }}
+      >
+        <ArrowBackIosNewRounded fontSize={size} color={color}  sx={{ fontSize: 20 }}/>
       </IconButton>
-      <IconButton onClick={onNext} size="small">
-        <ArrowCircleRightRounded fontSize={size} color={color} />
+
+      <IconButton
+        onClick={onNext}
+        size="medium"
+        sx={{
+          borderRadius: "50%",
+          bgcolor: "background.paper",
+          "&:hover": {
+            bgcolor: "action.hover",
+          },
+        }}
+      >
+        <ArrowForwardIosRounded fontSize={size} color={color} sx={{ fontSize: 20 }}/>
       </IconButton>
     </Box>
   );
