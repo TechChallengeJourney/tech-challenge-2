06f7f1e20a8a817fbd3ff6d21969890c08620bcf
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-07-26 20:59:09 -0300

    refactor: change alerts to design system snackbar

--- apps/bytebank-shell/src/pages/cards/card-bank-carousel/card-actions.tsx
+++ apps/bytebank-shell/src/pages/cards/card-bank-carousel/card-actions.tsx
@@ -1,6 +1,7 @@
-import { Box, Alert } from "@mui/material";
-import { BytebankButton } from "@repo/ui";
+import { Box } from "@mui/material";
+import { BytebankButton, BytebankSnackbar, SnackbarData } from "@repo/ui";
 import { useDeleteCard, useBlockCard } from "@repo/data-access";
+import { useState } from "react";
 
 interface CardActionsProps {
   cardId: string;
@@ -25,22 +26,79 @@ export const CardActions = ({
     loading: loadingBlock,
     blocked,
   } = useBlockCard(isBlocked);
-  const { handleDelete, loading: loadingDelete, error } = useDeleteCard();
+
+  const { handleDelete, loading: loadingDelete } = useDeleteCard();
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarData, setSnackbarData] = useState<SnackbarData | null>(null);
 
   const blockButtonLabel = getBlockButtonLabel(blocked, loadingBlock);
 
-  const handleBlockCard = async () => {
-    const success = await handleBlock(cardId);
-    if (success && onCardUpdate) onCardUpdate();
+  const showSnackbar = (data: SnackbarData) => {
+    console.log(data);
+    setSnackbarData(data);
+    setSnackbarOpen(true);
   };
 
-  const handleSetAsPrimary = () => {
-    alert("Marcado como principal");
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+    setSnackbarData(null);
+  };
+
+  const handleBlockCard = async () => {
+    try {
+      const success = await handleBlock(cardId);
+      console.log(success);
+      if (success) {
+        showSnackbar({
+          message: blocked
+            ? "Cartão desbloqueado com sucesso!"
+            : "Cartão bloqueado com sucesso!",
+          status: "success",
+        });
+        if (onCardUpdate)
+          setTimeout(() => {
+            onCardUpdate?.();
+          }, 1000);
+      } else {
+        showSnackbar({
+          message: "Não foi possível alterar o estado do cartão.",
+          status: "error",
+        });
+      }
+    } catch (error) {
+      showSnackbar({
+        message: "Erro ao bloquear/desbloquear o cartão.",
+        status: "error",
+      });
+    }
   };
 
   const handleDeleteCard = async () => {
-    const success = await handleDelete(cardId);
-    if (success && onCardUpdate) onCardUpdate();
+    try {
+      const success = await handleDelete(cardId);
+      console.log(success);
+      if (success) {
+        showSnackbar({
+          message: "Cartão excluído com sucesso!",
+          status: "success",
+        });
+        if (onCardUpdate)
+          setTimeout(() => {
+            onCardUpdate?.();
+          }, 1000);
+      } else {
+        showSnackbar({
+          message: "Não foi possível excluir o cartão.",
+          status: "error",
+        });
+      }
+    } catch (error) {
+      showSnackbar({
+        message: "Erro ao excluir o cartão.",
+        status: "error",
+      });
+    }
   };
 
   return (
@@ -51,11 +109,11 @@ export const CardActions = ({
       alignItems="center"
     >
       <Box width="100%">
-        {error && (
-          <Box mt={2} width="100%">
-            <Alert severity="error">{error}</Alert>
-          </Box>
-        )}
+        <BytebankSnackbar
+          open={snackbarOpen}
+          onClose={handleSnackbarClose}
+          data={snackbarData}
+        />
         <BytebankButton
           fullWidth
           variant="contained"
@@ -66,22 +124,7 @@ export const CardActions = ({
         />
       </Box>
 
-      <Box mt={2} width="100%">
-        <BytebankButton
-          fullWidth
-          color="primary"
-          variant="outlined"
-          label="Marcar cartão como principal"
-          onClick={handleSetAsPrimary}
-        />
-      </Box>
-
       <Box mt={2}>
-        {error && (
-          <Box mt={2} width="100%">
-            <Alert severity="error">{error}</Alert>
-          </Box>
-        )}
         <BytebankButton
           label={loadingDelete ? "Excluindo..." : "Excluir cartão"}
           color="primary"
