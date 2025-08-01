f0156e3d8441d7c442c23999b27558a086a7277f
Kauê <kauelivio@gmail.com>
2025-08-01 03:37:44 -0300

    fix: fixing horizontal scroll

--- apps/bytebank-shell/src/components/header/header.tsx
+++ apps/bytebank-shell/src/components/header/header.tsx
@@ -108,7 +108,7 @@ export function BytebankHeader() {
                 id="bytebank-header"
                 position={"sticky"}
                 sx={{
-                    width: "100vw",
+                    width: "100%",
                     transition: "all 0.3s ease",
                     boxShadow: "none",
                     borderBottom: "1px solid rgb(199 201 145 / 20%)",
